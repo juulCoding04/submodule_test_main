c767f0efecb66ce4c57f680bc9bac85e52fb4feb
Juul <juul20018@gmail.com>
2024-11-13 11:23:56 +0100

    adding submodules to the project and using the function from this module

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import math
+import submodule_test_1.math_tooling.return_calc as rc
 
 n1 = input('number 1: ')
 n2 = input('number 2: ')
 operation = input('sum/subtract/divide/multiply:')
 
-if operation == 'sum':
-    print(n1+n2)
-elif operation == 'subtract':
-    print(n1 - n2)
-elif operation == 'divide':
-    print(n1/n2)
-elif operation == 'multiply':
-    print(n1*n2)
-else:
-    print('invalid operation')
+calc = rc.return_calc(n1, n2, operation)
+print(calc)
