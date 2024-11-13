b707ed342e3301a4e2b167054d9940162c5269ed
Juul <juul20018@gmail.com>
2024-11-13 11:31:52 +0100

    using the new handy function from the submodule

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import math
 import submodule_test_1.math_tooling.return_calc as rc
+import submodule_test_1.math_tooling.handy_func as hf
 
 n1 = input('number 1: ')
 n2 = input('number 2: ')
-operation = input('sum/subtract/divide/multiply:')
+operation = input('sum/subtract/divide/multiply: ')
 
 calc = rc.return_calc(n1, n2, operation)
 print(calc)
+
+hf.handy_func()
