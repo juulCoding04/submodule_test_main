d6561af228af1a4e0f59cb25a6064238807f972f
Juul <juul20018@gmail.com>
2024-11-18 20:28:35 +0100

    remove submodule?

--- lib/submodule_test_1/README.md
+++
@@ -1,2 +0,0 @@
-# submodule_test_1
-first submodule we want to use inside of submodule_test_main
--- lib/submodule_test_1/math_tooling/handy_func.py
+++
@@ -1,4 +0,0 @@
-def handy_func():
-    print("this function does something handy")
-    print("this part is changed from inside the main project")
-    return 0
